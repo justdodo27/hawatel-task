aaa0b2b26b34b895f3b46fe834c555b6c61fbf10
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-06-12 01:23:14 +0200

    Update CurrencyFetch class docstrings

--- currency.py
+++ currency.py
@@ -3,21 +3,57 @@ import json
 import typing
 import logging
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+
 
 class CurrencyFetch:
+    """
+    A class to represent a currency fetcher.
+
+    ...
+
+    Attributes
+    ----------
+    code : str
+        currency code
+    url : str
+        url from which the data will be fetched
+    headers : dict
+        header for request fetch
+
+
+    Methods
+    -------
+    _fetch():
+        Fetchs the currency data from NBP API.
+    """
     def __init__(self, *, code: str):
+        """
+        Constructs all the necessary atributes for the CurrencyFetch object.
+
+        Parrameters
+        ----------
+            code : str
+                currency code
+        """
         self.code = code
-        self.table = 'a'
-        self.url = f'http://api.nbp.pl/api/exchangerates/rates/{self.table}/{self.code}/'
-        self.headers = {'Accept': 'application/json'}
+        self.url = (
+            f"http://api.nbp.pl/api/exchangerates/rates/a/{self.code}/"
+        )
+        self.headers = {"Accept": "application/json"}
 
     def _fetch(self) -> typing.Dict:
+        """
+        Fetchs the currency data from NBP API.
+
+        Returns:
+            data (dict): Dictionary data of the currency rate
+        """
         response = requests.get(self.url, headers=self.headers)
         if response.status_code == 200:
-            logging.info(f'Succesfully fetched exchange rate')
+            logging.info(f"Succesfully fetched exchange rate")
         else:
-            logging.error(f'API response with {response.status_code}')
+            logging.error(f"API response with {response.status_code}")
             return None
         data = json.loads(response.text)
 
